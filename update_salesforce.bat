ac9aef37b93e807c0d8404bbd60363ea9556c83b
Daniel Ramirez <danielan.ramirezs@ecci.edu.co>
2025-11-25 11:18:28 -0500

    Adding script and reorder structure

---
+++ update_salesforce.bat
@@ -0,0 +1,27 @@
+
+@echo off
+echo ============================================
+echo   Flujo Automatizado: Salesforce DX + Git
+echo ============================================
+
+REM 1. Retrieve desde Salesforce Org
+echo Recuperando metadatos desde el org Daniel...
+sf project retrieve start --manifest manifest/package.xml
+sf project retrieve start --classes
+
+REM 2. Agregar cambios a Git
+echo Agregando cambios a Git...
+git add .
+
+REM 3. Crear commit
+set /p msg="Update changes in Project Final "
+git commit -m "%msg%"
+
+REM 4. Hacer push a GitHub
+echo Subiendo cambios a GitHub de Daniel...
+git push origin main
+
+echo ============================================
+echo   ¡Proceso completado con éxito.....!
+echo ============================================
+pause
